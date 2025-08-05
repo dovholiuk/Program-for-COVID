cb5d5b92f9937283d56cec506ab83689ea16c078
Daryna Dovholiuk <ddaryna2005@gmail.com>
2025-08-05 20:25:09 +0300

    change count of falses

--- kod27.08.R
+++ kod27.08.R
@@ -17,30 +17,33 @@ for (pred in predictors) {
 
   fn_list <- list()
   fp_list <- list()
-  
+
   for (i in 1:100) {
     train_idx <- sample(1:n, k)
     train <- data[train_idx, ]
     test <- data[-train_idx, ]
+    global_idx <- setdiff(1:n, train_idx)
     
     m <- glm(train$Gr ~ train[[pred]], family = binomial)
     b_iter <- coef(m)
     ld_iter <- -b_iter[1] / b_iter[2]
     
     pred_class <- ifelse(test[[pred]] > ld_iter, 1, 0)
-    
-    fn_list[[i]] <- which(test$Gr == 1 & pred_class == 0)
-    fp_list[[i]] <- which(test$Gr == 0 & pred_class == 1)
+
+    fn_local <- which(test$Gr == 1 & pred_class == 0)
+    fp_local <- which(test$Gr == 0 & pred_class == 1)
+
+    fn_list[[i]] <- global_idx[fn_local]
+    fp_list[[i]] <- global_idx[fp_local]
   }
-  
+
   false_negatives[[pred]] <- fn_list
   false_positives[[pred]] <- fp_list
-
-  correct <- sum(pred_class == data$Gr)
-  incorrect <- sum(pred_class != data$Gr)
+  
 }
 
-cat("Correct:", toString(correct), "\n")
-cat("Incorrect:", toString(incorrect), "\n")
-cat("False Negatives:", toString(false_negatives), "\n")
-cat("False Positives:", toString(false_positives), "\n")
+false_negatives[["NLR"]]
+false_negatives[["d.NLR"]]
+false_negatives[["IGLR.100"]]
+false_negatives[["PLR"]]
+false_negatives[["SII"]]
